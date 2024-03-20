813307eb95abaa4905d2dce7e157f4a90778110b
Steven Surjadi <st.surjadi@gmail.com>
2024-03-20 16:33:24 +0700

    feat: adaptive platform

--- lib/widget/create_expense.dart
+++ lib/widget/create_expense.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/model/expense.dart';
 
@@ -41,18 +44,33 @@ class _CreateExpenseState extends State<CreateExpense> {
       isAmounInvalid || 
       _selectedDate == null
     ) {
-      showDialog(context: context, builder: (ctx) => AlertDialog(
-        title: const Text('Invalid input.'),
-        content: const Text('Please fill the field correctly!'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(ctx);
-            }, 
-            child: const Text('Okay')
-          )
-        ],
-      ));
+      if (Platform.isIOS) {
+        showCupertinoDialog(context: context, builder: (ctx) => CupertinoAlertDialog(
+          title: const Text('Invalid input.'),
+          content: const Text('Please fill the field correctly!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              }, 
+              child: const Text('Okay')
+            )
+          ],
+        ));
+      } else {
+        showDialog(context: context, builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input.'),
+          content: const Text('Please fill the field correctly!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              }, 
+              child: const Text('Okay')
+            )
+          ],
+        ));
+      }
       return;
     }
 
